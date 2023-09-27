6f034f4216aeff7f883f3f3db58167796db67fb0
Utkarsh SInha <vishalsingh@Vishals-MacBook-Air.local>
2023-09-27 19:51:43 +0530

    onboarding 1

--- app/src/main/java/com/example/headline_hub/onboarding_two.kt
+++ app/src/main/java/com/example/headline_hub/onboarding_two.kt
@@ -1,17 +1,11 @@
 package com.example.headline_hub
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.WindowManager
 
-class MainActivity : AppCompatActivity() {
+class onboarding_two : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_onboarding_two)
     }
 }
